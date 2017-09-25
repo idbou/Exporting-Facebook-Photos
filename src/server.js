752d65a5ac83dc49db971b6d088dc5153a3ca5cb
idbou <32271295+idbou@users.noreply.github.com>
2017-09-25 18:19:08 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -32,12 +32,7 @@ passport.use(new Strategy({
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
+      return cb(null, profile);
   }));
 
 passport.serializeUser(function(user, cb) {
